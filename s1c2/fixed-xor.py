9bc5c2944758617b6124586a8a43451e4be3c8f6
nirmal91 <nirmal.utwani@gmail.com>
2021-01-24 12:00:59 +0100

    Manual xor implementation.

--- s1c2/fixed-xor.py
+++ s1c2/fixed-xor.py
@@ -5,6 +5,19 @@ def xor(str1, str2):
     return bytes(a ^ b for (a,b) in zip(str1, str2))
 
 
+def manual_xor(str1, str2):
+    assert len(str1) == len(str2)
+    result_bytes = []
+    for i in range(len(str1)):
+        byte1 = str1[i]
+        byte2 = str2[i]
+
+        result_byte = byte1 ^ byte2
+        result_bytes.append(result_byte)
+
+    return bytes(result_bytes)
+
+
 if __name__ == '__main__':
     str1 = "1c0111001f010100061a024b53535009181c"
     str2 = "686974207468652062756c6c277320657965"
@@ -14,6 +27,6 @@ if __name__ == '__main__':
     print(str1_decoded)
     print(str2_decoded)
 
-    result = xor(hex_decode(str1), hex_decode(str2))
+    result = manual_xor(hex_decode(str1), hex_decode(str2))
     # encode it back to hex
     print(bytes.hex(result))
